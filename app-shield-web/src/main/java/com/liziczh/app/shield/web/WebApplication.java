430eb4fe85cb96bfe099c23ef6253b7c3082b450
liziczh <liziczh@foxmail.com>
2020-11-26 17:52:24 +0800

    Application

--- app-shield-web/src/main/java/com/liziczh/app/shield/web/WebApplication.java
+++ app-shield-web/src/main/java/com/liziczh/app/shield/web/WebApplication.java
@@ -3,6 +3,7 @@ package com.liziczh.app.shield.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.liziczh.app.shield.internal.InternalApplication;
 import com.liziczh.app.shield.mybatisplus.MybatisPlusApplication;
 import com.liziczh.app.shield.ref.RefApplication;
 import com.liziczh.app.shield.service.ServiceApplication;
@@ -10,7 +11,7 @@ import com.liziczh.app.shield.service.ServiceApplication;
 @SpringBootApplication(scanBasePackages = { "com.liziczh.*" })
 public class WebApplication {
 	public static void main(String[] args) {
-		Class<?>[] sources = new Class<?>[] { MybatisPlusApplication.class, ServiceApplication.class, RefApplication.class, WebApplication.class };
+		Class<?>[] sources = new Class<?>[] { MybatisPlusApplication.class, RefApplication.class, InternalApplication.class, ServiceApplication.class, WebApplication.class };
 		SpringApplication.run(sources, args);
 	}
 }
